e04efe01f53546defc4391730599e6fb046c664a
marcellopmartins <marcellopmartins@gmail.com>
2022-05-25 16:14:40 -0300

    trabalhando com módulos no javascript

---
+++ 003-Modulos-em-JavaScript/atividade/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Marcello', 49));
+
+console.log(mostraCidade('Marcello', 'Rio de Janeiro'));
+
+console.log(mostraHobby('Marcello', 'Ouvir música'));
